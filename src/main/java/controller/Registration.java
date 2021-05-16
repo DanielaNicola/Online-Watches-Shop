a825d316265a57c4b5cc677e38bca764cafe21df
Daniela <nicola.daniela3103@gmail.com>
2021-05-16 17:12:00 +0300

    Registration

---
+++ src/main/java/controller/Registration.java
@@ -0,0 +1,37 @@
+package controller;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import exceptions.UsernameAlreadyExists;
+import service.UserService;
+
+public class Registration {
+    @FXML
+    private Text registrationMessage;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private ChoiceBox role;
+
+    @FXML
+    public void initialize() {
+        role.getItems().addAll("Customer", "Seller");
+    }
+
+    @FXML
+    public void handleRegisterAction() {
+        try {
+            UserService.addUser(usernameField.getText(), passwordField.getText(), (String) role.getValue());
+            registrationMessage.setText("Account created successfully!");
+        } catch (UsernameAlreadyExists e) {
+            registrationMessage.setText(e.getMessage());
+        }
+    }
+
+}
